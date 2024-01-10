fe06cf45f040126fb953c00ab228089dc2cecccb
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:41:12 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "Holaaaaaaaa",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Diplomacia en la Era Global: Desafíos y Oportunidades",
+"autor" => "Dra. Isabel Mendoza",
+"resumen" => "En su último informe, la Dra. Isabel Mendoza, una reconocida analista internacional, examina los complejos desafíos y las oportunidades emergentes en el escenario diplomático global. El mundo se encuentra en un punto de inflexión, marcado por tensiones geopolíticas, la pandemia global y la urgente necesidad de colaboración internacional.",
 ];
 ?>
