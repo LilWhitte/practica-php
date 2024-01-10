5c726360c72e93061d85733ac108b6b4703928f4
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:41:45 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Desafíos y Oportunidades en la Actualidad Peruana: Perspectivas para el Desarrollo Nacional",
+"autor" => "Dra. Carmen Huertas",
+"resumen" => "En este análisis exhaustivo, la Dra. Carmen Huertas, destacada analista política peruana, examina la situación actual de Perú, abordando una variedad de desafíos y oportunidades que enfrenta la nación en su camino hacia el desarrollo. Este informe proporciona una visión panorámica de los temas clave que moldean el panorama nacional.",
 ];
 ?>
