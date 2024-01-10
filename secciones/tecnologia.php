d96c1f8e842531f65d1269adbcd69bfec7946a0a
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:46:42 -0500

    Update tecnologia.php

--- secciones/tecnologia.php
+++ secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Tecnología en Evolución: Tendencias que Moldearán el Futuro Digital",
+"autor" => "Alejandro Torres",
+"resumen" => "La velocidad vertiginosa a la que avanza la tecnología continúa transformando radicalmente la forma en que vivimos, trabajamos y nos relacionamos. Al observar las tendencias actuales, es evidente que nos encontramos en un punto de inflexión emocionante que delineará el panorama tecnológico en los próximos años.",
 ];
 ?>
