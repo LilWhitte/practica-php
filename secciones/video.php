8e0d682315669db4744057eb8a6e49a3ba8e01e6
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:47:28 -0500

    Update video.php

--- secciones/video.php
+++ secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La Revolución del Contenido Audiovisual: Nuevos Horizontes en el Mundo del Video",
+"autor" => "Carlos Vargas",
+"resumen" => "En la era actual, el consumo de video ha experimentado una metamorfosis sorprendente, transformando la manera en que nos entretenemos, aprendemos y nos conectamos con el mundo. La proliferación de plataformas y la evolución de la tecnología han allanado el camino para una nueva era en el mundo del video.",
 ];
 ?>
