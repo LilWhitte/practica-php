12c638f5962ae9d18d13981071589bfee04086cc
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:45:56 -0500

    Update sociedad.php

--- secciones/sociedad.php
+++ secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Desafíos Sociales en la Actualidad: Hacia una Transformación Colectiva",
+"autor" => "Elena Martínez",
+"resumen" => "La sociedad contemporánea se enfrenta a una serie de desafíos significativos que demandan nuestra atención y acción colectiva. Desde la desigualdad económica hasta la crisis climática, la urgencia de abordar estos problemas requiere un compromiso firme por parte de la sociedad en su conjunto.",
 ];
 ?>
