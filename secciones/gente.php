3c7bd8e3c8292aab9dacb824e01466b8051d19db
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:40:02 -0500

    Update gente.php

--- secciones/gente.php
+++ secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Historias Inspiradoras: Personas Comunes que Han Cambiado el Mundo",
+"autor" => "Sandra Gutiérrez",
+"resumen" => "En una serie de reportajes conmovedores, la periodista Sandra Gutiérrez ha explorado las vidas de personas comunes cuyas acciones extraordinarias han tenido un impacto significativo en el mundo que les rodea. Estas historias inspiradoras demuestran el poder transformador que puede surgir de la determinación, la compasión y la dedicación.",
 ];
 ?>
