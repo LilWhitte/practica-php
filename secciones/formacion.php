e8f5e20d9224d24dd51971be17c538130da5fad9
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:39:22 -0500

    Update formacion.php

--- secciones/formacion.php
+++ secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "Gabinete de comunicación",
-"resumen" => "XXX",
+"titulo" => "Revolución en la Formación Profesional: Aprendizaje en Línea y Tecnologías Emergentes",
+"autor" => "Prof. Andrés Torres,",
+"resumen" => "En un cambio significativo en el paradigma educativo, el Prof. Andrés Torres ha liderado iniciativas transformadoras en la formación profesional mediante la incorporación de tecnologías emergentes y métodos de aprendizaje en línea. Su enfoque innovador ha redefinido la manera en que las personas adquieren nuevas habilidades y conocimientos en el siglo XXI.",
 ];
 ?>
