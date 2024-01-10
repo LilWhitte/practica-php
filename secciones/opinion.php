fba21612d9bb566ef0ac198640c2689b075a3b59
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:44:57 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El Papel Crucial de la Empatía en un Mundo Conectado",
+"autor" => "Ana Rodríguez",
+"resumen" => "En un mundo cada vez más conectado y digitalizado, la empatía emerge como un valor fundamental que debemos cultivar y promover. La capacidad de comprender y compartir los sentimientos de los demás se vuelve crucial en un entorno donde las interacciones humanas a menudo se reducen a mensajes de texto, correos electrónicos y publicaciones en redes sociales.",
 ];
 ?>
