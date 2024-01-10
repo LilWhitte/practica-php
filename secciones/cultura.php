03f75dfdb6097db343c1a6c21c65c88d68cadf4a
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:36:41 -0500

    Update cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Redescubriendo las Tradiciones Culturales: Celebración Multicultural une a Comunidades",
+"autor" => "Dr. Alejandro Ramírez",
+"resumen" => "En un esfuerzo por preservar y celebrar la riqueza de la diversidad cultural, el Dr. Alejandro Ramírez, destacado antropólogo cultural, lideró una iniciativa que ha resultado en una Celebración Multicultural única. Este evento reunió a comunidades de diversas etnias, religiones y tradiciones en un intercambio enriquecedor de experiencias culturales.",
 ];
 ?>
