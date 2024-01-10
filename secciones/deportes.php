96d890c108b288a75e3a9bf458e5f81a2dbbfa19
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:37:18 -0500

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Récord Histórico en el Mundo del Deporte: Atleta Rompe Barrera de los 100 Metros",
+"autor" => "Carlos Martínez",
+"resumen" => "En un hito que ha dejado a la comunidad deportiva asombrada, el atleta de velocidad Jorge González ha logrado romper la barrera de los 100 metros en un tiempo extraordinario, estableciendo un nuevo récord mundial. El hecho tuvo lugar en el marco de una competición internacional, donde González superó las expectativas y desafió los límites de lo que se creía posible.",
 ];
 ?>
