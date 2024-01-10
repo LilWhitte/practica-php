6203ef2adc4b3ed228129418df72b2a3fd9615c4
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:35:45 -0500

    Update contenedores.php

--- secciones/contenedores.php
+++ secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Avances revolucionarios en la gestión de contenedores impulsan la eficiencia del desarrollo de software",
+"autor" => "Ing. Ana Torres",
+"resumen" => "En un hito significativo para la industria del desarrollo de software, la ingeniera Ana Torres ha liderado un equipo que ha logrado avances revolucionarios en la gestión de contenedores. Su investigación se centra en optimizar la eficiencia y escalabilidad de las aplicaciones mediante el uso avanzado de tecnologías de contenedores, como Docker y Kubernetes.",
 ];
 ?>
