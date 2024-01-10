00e9398e2de72b861c46d8a1b7e40eccdc3fedc3
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:44:21 -0500

    Update openstack.php

--- secciones/openstack.php
+++ secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Arquitecturas Cloud Empresariales: Un Enfoque Profundo en OpenStack",
+"autor" => " Dr. Javier Sánchez",
+"resumen" => "En este informe técnico, el Dr. Javier Sánchez, un reconocido arquitecto de sistemas en el ámbito de la nube, profundiza en OpenStack como una solución integral para construir y gestionar infraestructuras cloud empresariales. OpenStack ha emergido como una plataforma de código abierto que permite a las empresas crear entornos cloud privados y públicos altamente escalables.",
 ];
 ?>
