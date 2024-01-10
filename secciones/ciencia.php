963054ab6263edab2bd170a9bbad45f0fbadd2fe
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:34:53 -0500

    Update ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubrimiento revolucionario en la búsqueda de exoplanetas habitables",
+"autor" => "Dr. Elena Martínez",
+"resumen" => "En un emocionante avance científico, un equipo de investigadores liderado por la destacada astrofísica Dr. Elena Martínez ha anunciado el descubrimiento de varios exoplanetas que podrían albergar condiciones propicias para la vida. Utilizando observatorios espaciales avanzados, el equipo identificó planetas en la "zona habitable" alrededor de estrellas distantes, donde las temperaturas permiten la existencia de agua líquida.",
 ];
 ?>
