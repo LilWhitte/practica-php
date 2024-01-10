27b5dd3a0e59bd8b57b0bc9660798a8ecba886ad
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:38:05 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Innovación Financiera: Criptomonedas Impulsan un Cambio en el Paisaje Económico",
+"autor" => "Dra. María Fernández",
+"resumen" => "En un giro notable para el mundo financiero, las criptomonedas han emergido como una fuerza transformadora en el paisaje económico global. Bajo la dirección de la destacada economista Dra. María Fernández, un estudio exhaustivo ha revelado cómo estas monedas digitales están alterando las prácticas económicas tradicionales y desafiando las estructuras financieras convencionales.",
 ];
 ?>
