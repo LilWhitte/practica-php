07ed1e87e6cc9e3e452c898c27fb8c34f9f9c4d3
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:47:09 -0500

    Update television.php

--- secciones/television.php
+++ secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La Transformación Continua de la Televisión: Más Allá de las Pantallas Tradicionales",
+"autor" => "Lucía Martínez",
+"resumen" => "La industria televisiva está experimentando una transformación fascinante que va más allá de la simple evolución de la tecnología. Si bien las pantallas tradicionales siguen siendo relevantes, la forma en que consumimos contenido televisivo ha experimentado un cambio notable.",
 ];
 ?>
