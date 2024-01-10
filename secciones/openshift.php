96ac3b4876dd73cc2dcce5a3c867b7ab52363549
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:42:51 -0500

    Update openshift.php

--- secciones/openshift.php
+++ secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Transformación Digital Empresarial: Explorando las Posibilidades con OpenShift",
+"autor" => "Ing. Marta Ramírez",
+"resumen" => "En este informe especializado, la Ing. Marta Ramírez, experta en tecnologías de contenedores y plataformas de despliegue, profundiza en las oportunidades y beneficios que ofrece OpenShift en el contexto de la transformación digital empresarial. OpenShift, una plataforma de contenedores desarrollada por Red Hat, ha emergido como una herramienta clave para la implementación y gestión eficiente de aplicaciones en entornos de contenedores.",
 ];
 ?>
