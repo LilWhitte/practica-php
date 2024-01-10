66ac6b156c157632ee1e0b2ce8da2a5cd83fa818
Carlo Steffano Arróspide Gambini <75133106+PoIarBe4r@users.noreply.github.com>
2024-01-09 22:40:38 -0500

    Update git.php

--- secciones/git.php
+++ secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Dominando Git: Estrategias Avanzadas para un Control de Versiones Eficiente",
+"autor" => "Carlos Sánchez",
+"resumen" => "En su nuevo libro, el desarrollador senior y experto en Git, Carlos Sánchez, comparte estrategias avanzadas para aprovechar al máximo el sistema de control de versiones Git. Este recurso exhaustivo no solo es ideal para desarrolladores experimentados, sino que también ofrece valiosos conocimientos para aquellos que deseen profundizar en las capacidades más avanzadas de Git.",
 ];
 ?>
